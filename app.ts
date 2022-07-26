f5c676e6325545c85444a1a1c20de48fce54c035
awynnejo <89365137+awynnejo@users.noreply.github.com>
2022-07-26 17:05:46 +1000

    Grid approach - can't set canvas width dynamically

--- app.ts
+++ app.ts
@@ -30,35 +30,42 @@ class GameCanvas {
     game_size: number
     game: Game
     canvas: HTMLCanvasElement
-    canvas_width: number
     tile_size: number
     ctx: CanvasRenderingContext2D
 
-    constructor(){
-        this.game_size = 10
+    constructor(game_size: number = 10){
+        this.game_size = game_size
         this.game = new Game(this.game_size)
-        this.canvas_width = 500
-        this.tile_size = this.canvas_width / 10
         this.canvas = <HTMLCanvasElement>document.getElementById('canvas')
+        this.tile_size = this.canvas.width / this.game_size
         this.ctx = this.canvas.getContext("2d")!
+        this.canvas.addEventListener('click', event => {
+            const x = (event.screenX - this.canvas.getBoundingClientRect().left) / this.tile_size
+            const y = (event.screenY - this.canvas.getBoundingClientRect().top) / this.tile_size
+            console.log(x + ':' + y)
+            console.log(this.tile_size)
+        }, false)
+        window.addEventListener("resize", this.updateTileSize()!, false)
     }
-    getGridCoordinates(){
-       return null
+
+    updateTileSize(){
+        this.tile_size = (this.canvas.getBoundingClientRect().right - this.canvas.getBoundingClientRect().left) / this.game_size
     }
 
     drawBoard(){
-        for(let i = 0; i < this.canvas_width; i++){
+        for(let i = 0; i < this.canvas.width; i++){
             this.ctx.beginPath()
             this.ctx.moveTo(i * this.tile_size, 0)
-            this.ctx.lineTo(i * this.tile_size, this.canvas_width)
+            this.ctx.lineTo(i * this.tile_size, this.canvas.width)
             this.ctx.stroke()
             this.ctx.moveTo(0, i * this.tile_size)
-            this.ctx.lineTo(this.canvas_width, i * this.tile_size)
+            this.ctx.lineTo(this.canvas.width, i * this.tile_size)
             this.ctx.stroke()
 
         }
     }
+
 }
 
-let gc = new GameCanvas()
+let gc = new GameCanvas(10)
 gc.drawBoard()
